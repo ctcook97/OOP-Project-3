b0352226642155a13c38f5c4aa1698f79f092032
ctcook97 <ctcook97@gmail.com>
2019-03-26 12:07:52 -0400

    2 player fields

--- src/main/course/oop/tictactoe/view/MainView.java
+++ src/main/course/oop/tictactoe/view/MainView.java
@@ -33,13 +33,7 @@ public class MainView {
 	public Scene getMainScene() {
 		return this.scene;
 	}
-	
-	/**
-	 * The setup pane is where a user can give input
-	 * for the initialization of the 2D array.
-	 * 
-	 * @return
-	 */
+
 	public GridPane buildSetupPane() {
 	    Text sizeLabel = new Text("Number of players:");  
         TextField sizeTextField = new TextField();
@@ -101,12 +95,12 @@ public class MainView {
 		}
 	}
 	
-	public GridPane enter2PlayerNames() {
-	    Text sizeLabel = new Text("Player 1 name:");  
-	    Text defaultValLabel = new Text("Player 2 name:");       
-        TextField sizeTextField = new TextField();
-        //TODO #1: Add a text field for a user to input a default value to init array
-        TextField dvTextField = new TextField();
+	public GridPane enter1PlayerName() {
+	    Text p1nameLabel = new Text("Player 1 name:");  
+	    Text p1markerLabel = new Text("Player 1 marker");       
+        TextField p1nameField = new TextField();
+        TextField p1markerField = new TextField();   
+        
         Button button1 = new Button("Submit"); 
         button1.getStyleClass().add("orangeButton");
         Line line = new Line();
@@ -121,8 +115,8 @@ public class MainView {
         EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() { 
            @Override 
            public void handle(MouseEvent e) { 
-               String size = sizeTextField.getText();
-               String defaultVal = dvTextField.getText();
+               String size = p1nameField.getText();
+               String defaultVal = p1markerField.getText();
 
                System.out.println(size + defaultVal);
            } 
@@ -146,16 +140,80 @@ public class MainView {
         //Setting the Grid alignment 
         gridPane.setAlignment(Pos.CENTER); 
         
-        gridPane.add(sizeLabel, 0, 0); 
-        //TODO #3: Remove comment so that the label will show
-        gridPane.add(defaultValLabel, 1, 0); 
+        gridPane.add(p1nameLabel, 0, 0); 
+        gridPane.add(p1markerLabel, 1, 0); 
         
-        gridPane.add(sizeTextField, 0, 1); 
+        gridPane.add(p1nameField, 0, 1); 
         
-        //TODO #4: Add the text field for the default value
-        gridPane.add(dvTextField, 1, 1);
+        gridPane.add(p1markerField, 1, 1);
         gridPane.add(button1, 2, 1); 
         gridPane.add(line, 0, 2, 3, 1); 
+              
+        return gridPane;
+	}
+	
+	public GridPane enter2PlayerNames() {
+	    Text p1nameLabel = new Text("Player 1 name:");  
+	    Text p1markerLabel = new Text("Player 1 marker");       
+        TextField p1nameField = new TextField();
+        TextField p1markerField = new TextField();   
+        
+        Text p2nameLabel = new Text("Player 2 name:");  
+	    Text p2markerLabel = new Text("Player 2 marker");       
+        TextField p2nameField = new TextField();
+        TextField p2markerField = new TextField();   
+        
+        Button button1 = new Button("Submit"); 
+        button1.getStyleClass().add("orangeButton");
+        Line line = new Line();
+        
+        
+        line.setStartX(0.0f); 
+        line.setStartY(0.0f);         
+        line.setEndX((float) windowWidth); 
+        line.setEndY(0.0f);
+        
+        //Creating the mouse event handler 
+        EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() { 
+           @Override 
+           public void handle(MouseEvent e) { 
+               String size = p1nameField.getText();
+               String defaultVal = p1markerField.getText();
+
+               System.out.println(size + defaultVal);
+           } 
+        };  
+        //Registering the event filter 
+        button1.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);   
+
+        //Creating a Grid Pane 
+        GridPane gridPane = new GridPane();    
+        
+        //Setting size for the pane 
+        gridPane.setMinSize(windowWidth, (int) windowHeight/4); 
+        
+        //Setting the padding  
+        gridPane.setPadding(new Insets(10, 10, 10, 10)); 
+        
+        //Setting the vertical and horizontal gaps between the columns 
+        gridPane.setVgap(5); 
+        gridPane.setHgap(5);       
+        
+        //Setting the Grid alignment 
+        gridPane.setAlignment(Pos.CENTER); 
+        
+        gridPane.add(p1nameLabel, 0, 0); 
+        gridPane.add(p1markerLabel, 1, 0); 
+        gridPane.add(p1nameField, 0, 1); 
+        gridPane.add(p1markerField, 1, 1);
+        
+        gridPane.add(p2nameLabel, 0, 2); 
+        gridPane.add(p2markerLabel, 1, 2); 
+        gridPane.add(p2nameField, 0, 3); 
+        gridPane.add(p2markerField, 1, 3);
+        
+        gridPane.add(button1, 2, 3); 
+        gridPane.add(line, 0, 4, 3, 1); 
               
         return gridPane;
 	}
