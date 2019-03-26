243aba314a923da2c786b18315d9c2811dbdbbf1
ctcook97 <ctcook97@gmail.com>
2019-03-26 00:06:25 -0400

    clone of 2darray GUI

---
+++ src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import main.course.oop.tictactoe.view.MainView;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			MainView mView = new MainView();
+			Scene scene = mView.getMainScene();
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+			primaryStage.setTitle("Cameron Cook: 2D Array GUI Example");
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
