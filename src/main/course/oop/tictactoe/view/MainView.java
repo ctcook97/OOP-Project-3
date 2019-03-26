90a9d08694e2a904872a256aab29a77006a24567
ctcook97 <ctcook97@gmail.com>
2019-03-26 13:29:03 -0400

    Grid of squares works

--- src/main/course/oop/tictactoe/view/MainView.java
+++ src/main/course/oop/tictactoe/view/MainView.java
@@ -6,6 +6,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -35,9 +36,11 @@ public class MainView {
 	}
 
 	public GridPane buildSetupPane() {
-	    Text sizeLabel = new Text("Number of players:");  
-        TextField sizeTextField = new TextField();
-
+	    Text numPlayersLabel = new Text("Number of Players:");  
+	    Text timeoutLabel = new Text("Timeout in Seconds:");       
+        TextField numPlayersField = new TextField();
+        TextField timeoutField = new TextField();   
+        
         Button button1 = new Button("Submit"); 
         button1.getStyleClass().add("orangeButton");
         Line line = new Line();
@@ -51,8 +54,12 @@ public class MainView {
         EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() { 
            @Override 
            public void handle(MouseEvent e) { 
-               String numPlayers = sizeTextField.getText();
-               buildNameInputPane(numPlayers);
+               String numPlayers = numPlayersField.getText();
+               String timeout = timeoutField.getText();
+               System.out.println(numPlayers + timeout);
+//               buildNameInputPane(numPlayers);
+               root.setTop(gameView());
+               
            } 
         };  
         //Registering the event filter 
@@ -74,10 +81,10 @@ public class MainView {
         //Setting the Grid alignment 
         gridPane.setAlignment(Pos.CENTER); 
         
-        gridPane.add(sizeLabel, 0, 0); 
-        
-        gridPane.add(sizeTextField, 0, 1); 
-        
+        gridPane.add(numPlayersLabel, 0, 0); 
+        gridPane.add(timeoutLabel, 1, 0); 
+        gridPane.add(numPlayersField, 0, 1); 
+        gridPane.add(timeoutField, 1, 1);
         gridPane.add(button1, 2, 1); 
         gridPane.add(line, 0, 2, 3, 1); 
               
@@ -104,7 +111,6 @@ public class MainView {
         Button button1 = new Button("Submit"); 
         button1.getStyleClass().add("orangeButton");
         Line line = new Line();
-        
         
         line.setStartX(0.0f); 
         line.setStartY(0.0f);         
@@ -142,9 +148,7 @@ public class MainView {
         
         gridPane.add(p1nameLabel, 0, 0); 
         gridPane.add(p1markerLabel, 1, 0); 
-        
         gridPane.add(p1nameField, 0, 1); 
-        
         gridPane.add(p1markerField, 1, 1);
         gridPane.add(button1, 2, 1); 
         gridPane.add(line, 0, 2, 3, 1); 
@@ -214,6 +218,76 @@ public class MainView {
         
         gridPane.add(button1, 2, 3); 
         gridPane.add(line, 0, 4, 3, 1); 
+              
+        return gridPane;
+	}
+
+	
+	public GridPane gameView() {
+        
+        Button[][] squares = new Button[3][3];
+        for(int i = 0; i < 3; ++i) {
+        	for (int j = 0; j < 3; ++j) {
+        		squares[i][j] = new Button("X");
+        		squares[i][j].setMinWidth(150);
+        		squares[i][j].setMinHeight(150);
+        		squares[i][j].setMaxWidth(150);
+        		squares[i][j].setMaxHeight(150);
+        		squares[i][j].getStyleClass().add("orangeButton");
+        	}
+        }
+        
+//        line.setStartX(0.0f); 
+//        line.setStartY(0.0f);         
+//        line.setEndX((float) windowWidth); 
+//        line.setEndY(0.0f);
+//        
+        //Creating the mouse event handler 
+        EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() { 
+           @Override 
+           public void handle(MouseEvent e) { 
+        	   Button temp = (Button) e.getSource();
+        	   for(int i = 0; i < 3; ++i) {
+        		   for(int j = 0; j < 3; ++j) {
+        			   if(squares[i][j] == temp) {
+        				   System.out.println(i + " " + j);
+        			   }
+        		   }
+               }
+               System.out.println("ayo");
+           } 
+        };  
+        //Registering the event filter 
+        for(int i = 0; i < 3; ++i) {
+        	for(int j = 0; j < 3; ++j) {
+        		squares[i][j].addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler); 
+        	}
+        }
+
+        //Creating a Grid Pane 
+        GridPane gridPane = new GridPane();    
+        
+        //Setting size for the pane 
+        gridPane.setMinSize(windowWidth, (int) windowHeight/4); 
+        
+        //Setting the padding  
+        gridPane.setPadding(new Insets(10, 10, 10, 10)); 
+        
+        //Setting the vertical and horizontal gaps between the columns 
+        gridPane.setVgap(5); 
+        gridPane.setHgap(5);       
+        
+        //Setting the Grid alignment 
+        gridPane.setAlignment(Pos.CENTER); 
+        
+//        gridPane.add(button1, 2, 3); 
+//        gridPane.add(line, 0, 4, 3, 1); 
+        
+        for(int i = 0; i < 3; ++i) {
+        	for(int j = 0; j < 3; ++j) {
+        		gridPane.add(squares[i][j], j, i);
+        	}
+        }
               
         return gridPane;
 	}
